12f2912eae03289796dfb144180471877d1b1fa1
honeyai <contact.jessicasanchez@gmail.com>
2021-11-23 22:47:02 -0500

    added nonfic carousel

--- front-end-for-practice/src/Components/Home/Home.js
+++ front-end-for-practice/src/Components/Home/Home.js
@@ -3,19 +3,22 @@ import React, {useEffect, useState} from 'react';
 import Carousel from '../Carousel/Carousel';
 import "./home.css"
 const Home = () => {
-  //!move the request calls here so that The carousel has no logic regarding what to display, only fed what to display.
+
   const [bestSellers, setBestSellers] = useState();
+  const [bestNonFic, setBestNonFic] = useState();
+
   useEffect(() => {
-    const getTitles = async () => {
+    const getBooks = async () => {
       try {
         const response = await fetch('http://localhost:8080/b/list');
         const body = await response.json();
         setBestSellers(body.filter((b) => b.rating > 3 ));
+        setBestNonFic(body.filter((b) => b.rating > 3 && b.genre === "non-fiction"));
       } catch (error) {
         console.error(error.message);
       }
     }
-    getTitles();
+    getBooks();
   }, []);
 
   return (
@@ -23,6 +26,10 @@ const Home = () => {
       <div className="home_BestSellersContainer">
         <Typography>Best Sellers</Typography>
         <Carousel books={bestSellers}/>
+      </div>
+      <div className="home_BestSellersContainer">
+        <Typography>Best Non-Fiction</Typography>
+        <Carousel books={bestNonFic}/>
       </div>
     </div>
   );
