a9818b6d4df9f87619b574df90794bb2e9bce089
honeyai <contact.jessicasanchez@gmail.com>
2021-11-25 20:30:53 -0500

    adding missed files

---
+++ front-end-for-practice/src/Components/LocationCard/LocationCard.js
@@ -0,0 +1,23 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+import "./locationCard.css";
+
+const LocationCard = ({ location, address, zip, hours }) => {
+
+  console.log(location)
+  return (
+    <div className="locationCard_Container">
+      <div className="locationCard_Card">
+        <div className="locationCard_Image" />
+        <div className="locationCard_WhereAndWhen">
+          <Typography> {location} </Typography>
+          <Typography> {address} </Typography>
+          <Typography> {zip} </Typography>
+          <Typography> {hours} </Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LocationCard;
